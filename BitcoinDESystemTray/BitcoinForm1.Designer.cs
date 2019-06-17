aaff66887b895411d5328c52ebf235533faf8dbe
djspacedevil <goesi@emsland-party.de>
2019-06-17 22:27:05 +0200

    [TASK] Public Bitcoin Price SystemTray for Windows

---
+++ BitcoinDESystemTray/BitcoinForm1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace BitcoinDESystemTray
+{
+    partial class BitcoinWindow
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(BitcoinWindow));
+            this.bitcoinIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openBitcoindeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importentToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bitcoinIcon
+            // 
+            this.bitcoinIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.bitcoinIcon.BalloonTipText = " ";
+            this.bitcoinIcon.BalloonTipTitle = "Bitcoin.de Prices";
+            this.bitcoinIcon.ContextMenuStrip = this.contextMenuStrip1;
+            this.bitcoinIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("bitcoinIcon.Icon")));
+            this.bitcoinIcon.Tag = " ";
+            this.bitcoinIcon.Text = "Bitcoin,de Buy: 0.00€ Sell: 0.00€ ~Price: 0.00€ (00:00:00h)";
+            this.bitcoinIcon.Visible = true;
+            this.bitcoinIcon.BalloonTipClicked += new System.EventHandler(this.bitcoinIcon_BalloonTipClicked);
+            this.bitcoinIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bitcoinIcon_MouseClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openBitcoindeToolStripMenuItem,
+            this.importentToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(160, 70);
+            // 
+            // openBitcoindeToolStripMenuItem
+            // 
+            this.openBitcoindeToolStripMenuItem.Name = "openBitcoindeToolStripMenuItem";
+            this.openBitcoindeToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.openBitcoindeToolStripMenuItem.Text = "Open Bitcoin.de";
+            this.openBitcoindeToolStripMenuItem.Click += new System.EventHandler(this.openBitcoindeToolStripMenuItem_Click);
+            // 
+            // importentToolStripMenuItem
+            // 
+            this.importentToolStripMenuItem.Name = "importentToolStripMenuItem";
+            this.importentToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.importentToolStripMenuItem.Text = "Importent";
+            this.importentToolStripMenuItem.Visible = false;
+            this.importentToolStripMenuItem.Click += new System.EventHandler(this.importentToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(64, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Copyright by Sven-Goessling.de";
+            // 
+            // BitcoinWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "BitcoinWindow";
+            this.Text = "Bitcoin.de Price Status";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon bitcoinIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openBitcoindeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importentToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BitcoinDESystemTray/BitcoinForm1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Threading;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace BitcoinDESystemTray
+{
+    public partial class BitcoinWindow : Form
+    {
+        //invisible form
+        protected override void SetVisibleCore(bool value)
+        {
+            if (!this.IsHandleCreated)
+            {
+                this.CreateHandle();
+                value = false;
+            }
+            base.SetVisibleCore(value);
+        }
+
+        /*protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+
+        }*/
+
+        public BitcoinWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                Thread myThread = new Thread(delegate () { bitcoinDE_run(); });
+                myThread.IsBackground = true;
+                myThread.Start();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Fatal Error: " + e.Message);
+                this.Close();
+            }
+        }
+
+        private void bitcoinDE_run()
+        {
+            try
+            {
+                string json;
+                string newToolTip = "Buy: 0.00€ Sell: 0.00€ ~Price: 0.00€ (" + DateTime.Now.Hour.ToString() + ":" + DateTime.Now.Minute.ToString() + ":" + DateTime.Now.Second.ToString() + "h)";
+                string importentMessage = "";
+                string importentLink = "";
+                string old_importentMessage = "";
+                string old_importentLink = "";
+                double buyPrice = 0;
+                double sellPrice = 0;
+                double averagePrice = 0;
+                double old_averagePrice = 0;
+
+                while (true)
+                {
+                    json = new TimedWebClient { Timeout = 20000 }.DownloadString("http://miner-control.de/bitcoin_de.json");
+                    if (json != "")
+                    {
+                        dynamic dynJSON = JsonConvert.DeserializeObject(json);
+                        buyPrice = ((dynJSON.buyPrice != null && dynJSON.buyPrice != "") ? Convert.ToDouble(dynJSON.buyPrice) : 0);
+                        sellPrice = ((dynJSON.sellPrice != null && dynJSON.sellPrice != "") ? Convert.ToDouble(dynJSON.sellPrice) : 0);
+                        averagePrice = ((dynJSON.averagePrice != null && dynJSON.averagePrice != "") ? Convert.ToDouble(dynJSON.averagePrice) : 0);
+                        old_averagePrice = ((dynJSON.averagePrice_3h != null && dynJSON.averagePrice_3h != "") ? Convert.ToDouble(dynJSON.averagePrice_3h) : 0);
+
+                        newToolTip = "Buy: " + string.Format("{0:N2}", buyPrice) + "€ Sell: " + string.Format("{0:N2}", sellPrice) + "€ ~Price: " + string.Format("{0:N2}", averagePrice) + "€ (" + ((dynJSON.lastUpdateTime != null && dynJSON.lastUpdateTime != "") ? dynJSON.lastUpdateTime : "xx:xx:xx") + "h)";
+                        importentMessage = ((dynJSON.importentMessage != null && dynJSON.importentMessage != "") ? dynJSON.importentMessage : "");
+                        importentLink = ((dynJSON.importentLink != null && dynJSON.importentLink != "") ? dynJSON.importentLink : "");
+                    }
+                    if (averagePrice > old_averagePrice)
+                    {
+                        bitcoinIcon.Icon = new Icon(GetType(), "bitcoin_green.ico");
+                    }
+                    else if (averagePrice < old_averagePrice)
+                    {
+                        bitcoinIcon.Icon = new Icon(GetType(), "bitcoin_red.ico");
+                    }
+                    else
+                    {
+                        bitcoinIcon.Icon = new Icon(GetType(), "bitcoin.ico");
+                    }
+                    old_averagePrice = averagePrice;
+
+                    //Info Ballon
+                    if (importentMessage != old_importentMessage && importentLink != old_importentLink && importentLink != "" && importentMessage != "")
+                    {
+                        bitcoinIcon.BalloonTipText = importentMessage + " : Click here to open.";
+                        bitcoinIcon.ShowBalloonTip(5000);
+                    }
+
+                    //Set all Infos to bar
+                    bitcoinIcon.BalloonTipText = bitcoinIcon.Text = newToolTip;
+
+                    if (importentMessage != "")
+                    {
+                        importentToolStripMenuItem.Text = importentMessage;
+                        importentToolStripMenuItem.Tag = importentLink;
+                        importentToolStripMenuItem.Visible = true;
+                    }
+                    else
+                    {
+                        importentToolStripMenuItem.Visible = false;
+                    }
+
+                    //Wait 1 min 
+                    Thread.Sleep(60000); //60000 = 1 min
+                    json = "";
+                    old_importentMessage = importentMessage;
+                    old_importentLink = importentLink;
+                }
+            }
+            catch (Exception e)
+            {
+                //MessageBox.Show("Fatal Error: " + e.Message);
+                bitcoinDE_run();
+            }
+
+
+        }
+
+        private class TimedWebClient : WebClient
+        {
+            // Timeout in milliseconds, default = 600,000 msec
+            public int Timeout { get; set; }
+
+            public TimedWebClient()
+            {
+                this.Timeout = 300000;
+            }
+
+            protected override WebRequest GetWebRequest(Uri address)
+            {
+                var objWebRequest = base.GetWebRequest(address);
+                objWebRequest.Timeout = this.Timeout;
+                return objWebRequest;
+            }
+        }
+
+        private void BitcoinWindow_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                this.ShowInTaskbar = false;
+            }
+        }
+
+        private void bitcoinIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+           if( e.Button == MouseButtons.Left) { 
+            bitcoinIcon.ShowBalloonTip(5000);
+                //this.Show();
+                //this.WindowState = FormWindowState.Normal;
+                ///this.ShowInTaskbar = true;
+                /// 
+                }
+            }
+
+        private void openBitcoindeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.bitcoin.de/de/r/v8tznb");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void importentToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (importentToolStripMenuItem.Tag != null && importentToolStripMenuItem.Tag.ToString() != "")
+            {
+                System.Diagnostics.Process.Start(importentToolStripMenuItem.Tag.ToString());
+            }
+        }
+
+        private void bitcoinIcon_BalloonTipClicked(object sender, EventArgs e)
+        {
+            if (importentToolStripMenuItem.Tag != null && importentToolStripMenuItem.Tag.ToString() != "")
+            {
+                System.Diagnostics.Process.Start(importentToolStripMenuItem.Tag.ToString());
+            }
+        }
+    }
+
+
+}
